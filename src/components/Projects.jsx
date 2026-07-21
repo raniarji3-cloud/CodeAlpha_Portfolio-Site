14855e25e48a56f07bc36a4273a5cff0c30ac17d
Rani Arji <raniarji3@gmail.com>
2026-07-21 19:22:25 +0530

    updated My  Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,7 +30,21 @@ function Projects() {
           <h2>Image Gallery</h2>
           <p>Responsive image gallery with animations.</p>
         </div>
-
+        <div className="project-card">
+          <img src={project4} alt="project4" />
+          <h2>E-commerce store</h2>
+          <p>AI based responsive e-commerce platform with payment integration.</p>
+        </div>
+        <div className="project-card">
+          <img src={project5} alt="project5" />
+          <h2>Project-Management System</h2>
+          <p>Responsive project management platform with task tracking.</p>
+        </div>
+        <div className="project-card">
+          <img src={project6} alt="project6" />
+          <h2>Restaurant-Management System</h2>
+          <p>Responsive restaurant management platform with order tracking.</p>
+        </div>
       </div>
 
     </section>
